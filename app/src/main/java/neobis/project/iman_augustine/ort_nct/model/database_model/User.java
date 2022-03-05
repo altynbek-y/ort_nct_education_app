04c01b16a427d760d5cdf88345cf8725e8ba43bb
nami_augustine <altynbek_y@auca.kg>
2022-03-06 01:42:47 +0600

    Prepared for adding foreign key references in entity classes

--- app/src/main/java/neobis/project/iman_augustine/ort_nct/model/database_model/User.java
+++ app/src/main/java/neobis/project/iman_augustine/ort_nct/model/database_model/User.java
@@ -2,9 +2,22 @@ package neobis.project.iman_augustine.ort_nct.model.database_model;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "user")
+@Entity(tableName = "user"
+        /*,
+        foreignKeys =
+                {
+                        @ForeignKey
+                                (
+                                    entity = Language.class,
+                                    parentColumns = "language",
+                                    childColumns = "language",
+                                    onDelete = ForeignKey.CASCADE
+                                )
+                }*/
+)
 public class User {
     @PrimaryKey
     @ColumnInfo(name = "id")
